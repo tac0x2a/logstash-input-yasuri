f2dd91752315b5183a2474d8da5413db77f150cb
TAC <tac@tac42.net>
2016-12-13 00:56:19 +0900

    Available json file as parse_tree

--- spec/inputs/yasuri_spec.rb
+++ spec/inputs/yasuri_spec.rb
@@ -30,6 +30,14 @@ describe LogStash::Inputs::Yasuri do
     )
   }
 
+  let(:input_from_parse_tree_file) {
+    input_from_parse_tree_file = LogStash::Plugin.lookup("input", "yasuri").new(
+      "split" => true,
+      "url" => "https://news.ycombinator.com/",
+      "parse_tree_path" => "spec/inputs/res/parse_tree.json"
+    )
+  }
+
   it "should register" do
     # register will try to load jars and raise if it cannot find jars or if org.apache.log4j.spi.LoggingEvent class is not present
     expect {input.register}.to_not raise_error
@@ -39,6 +47,12 @@ describe LogStash::Inputs::Yasuri do
   it "enqueues some events" do
     input.register
     input.inner_run(queue)
+    expect(queue.size).not_to be_zero
+  end
+
+  it "enqueues some events from input_from_parse_tree_file" do
+    input_from_parse_tree_file.register
+    input_from_parse_tree_file.inner_run(queue)
     expect(queue.size).not_to be_zero
   end
 end
